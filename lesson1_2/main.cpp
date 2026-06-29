1e9c831ebbf5691a06a4287ffc28245d854e1b7c
Dayswh1029 <hengwang1029@gmail.com>
2026-06-29 10:41:12 +0200

    Version 0.0.1

---
+++ lesson1_2/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+// 头文件的作用其实是为了展开！ 头文件里面的代码 
+#include "add.h"
+
+int main()
+{
+
+
+
+   int sum=add(2,4);
+   std::cout<<sum<<std::endl;
+
+    return 0;
+}
---
+++ lesson2_1/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+#include "./lib/add.h"
+
+int main()
+{
+
+
+
+   int sum=add(2,4);
+   std::cout<<sum<<std::endl;
+
+    return 0;
+}
